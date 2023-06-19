cf5b58109446f3fa091b74726ba17802880fdaca
DeadChannelDice <michael.charpin@gmail.com>
2023-06-19 13:35:25 -0400

    Built out logic for gameplay.

--- main.js
+++ main.js
@@ -7,27 +7,69 @@ const result = document.querySelector(".result");
 
 // Generate a random selection for the machine //
 // Create an array w/ different choices and have a random number generator select an option // 
-const machine = [rock, paper, scissors]
+const machine = ["rock", "paper", "scissors"]
 
-rock.addEventListener('click', function () {    
-   const randomNumber = getRandomNumber()
-    console.log(randomNumber)
-    });
-
-
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * machine.length);
+const machineResponse = () => {
+    return machine[Math.floor(Math.random() * machine.length)];
 }
-
 
 
 // Input the user choice //
 
+    rock.addEventListener('click', () => {    
+        let manWeapon = "rock"
+        let machineWeapon = machineResponse() 
+        battle(manWeapon, machineWeapon)
+         })
+     
+     paper.addEventListener('click', () => {
+        let manWeapon = "paper"
+        let machineWeapon = machineResponse() 
+        battle(manWeapon, machineWeapon)
+     })
+     
+     scissors.addEventListener('click', () => {
+        let manWeapon = "scissors"
+        let machineWeapon = machineResponse() 
+        battle(manWeapon, machineWeapon)
+     })
+
+
 
 // Compare user choice to machine choice //
+const battle = (manWeapon, machineWeapon) => {
+    if (manWeapon == machineWeapon) {
+        tie()
+    } else if (
+        manWeapon == "rock" && machineWeapon == "paper" 
+        || manWeapon == "paper" && machineWeapon == "scissors"
+        || manWeapon == "scissors" && machineWeapon == "rock" 
+        ){
+            defeat()
+    } else if (
+        manWeapon == "rock" && machineWeapon == "scissors"
+        || manWeapon == "paper" && machineWeapon == "rock"
+        || manWeapon == "scissors" && machineWeapon == "paper"
+        ){
+            victory()
+    } 
+
+
+}
 
 // Generate win/ lose/ tie/ results //
+const victory = () => {
+    result.textContent = "You're doing great!"
+}
+
+const defeat = () => { 
+    result.textContent = "You'll get'em next time!"
+}
+
+const tie = () => {
+    result.textContent = "Keep at it!"
+}
+
 
 // Create play again? button // 
 
